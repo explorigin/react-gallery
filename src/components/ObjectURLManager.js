e71ac7421b0d752cc6db4891c0e9981b0c61b263
Timothy Farrell <tim@thecookiejar.me>
2017-01-07 04:31:51 -0600

    Fix ObjectURLManager to better manage URLs in component lifecycles

--- src/components/ObjectURLManager.js
+++ src/components/ObjectURLManager.js
@@ -1,11 +1,39 @@
 import { Component } from 'react';
 
 export default class ObjectURLManager extends Component {
-	componentWillUnmount() {
-		for (let url of ObjectURLManager.urls.values()) {
+	static urls = new Map();
+
+	static cleanup(id) {
+		let url = ObjectURLManager.urls.get(id);
+
+		if (url) {
 			URL.revokeObjectURL(url);
+			ObjectURLManager.urls.delete(id);
 		}
-		ObjectURLManager.urls.clear();
+	}
+
+	constructor() {
+		super();
+
+		this.localIds = new Set();
+		this.oldIds = null;
+	}
+
+	componentWillUpdate() {
+		this.oldIds = this.localIds;
+		this.localIds = new Set();
+	}
+
+	componentDidUpdate() {
+		const toRemove = Array.from(this.oldIds).filter(id => !this.localIds.has(id));
+
+		toRemove.forEach(ObjectURLManager.cleanup);
+		this.oldIds = null;
+	}
+
+	componentWillUnmount() {
+		Array.from(this.localIds).forEach(ObjectURLManager.cleanup);
+		this.localIds.clear();
 	}
 
 	getObjectUrl(id, img) {
@@ -13,9 +41,9 @@ export default class ObjectURLManager extends Component {
 		if (!url) {
 			url = URL.createObjectURL(img.data);
 			ObjectURLManager.urls.set(id, url);
+			this.localIds.add(id);
 		}
+
 		return url;
 	}
 }
-
-ObjectURLManager.urls = new Map();
